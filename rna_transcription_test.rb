ca2b30cd2f61ffbdacb65f13ae1e5a954b599ed2
Fabienne <fabienne@fidzup.com>
2019-07-10 20:13:49 +0200

    4 first tests : Complement class and of_dna method implementation

--- rna_transcription_test.rb
+++ rna_transcription_test.rb
@@ -8,22 +8,18 @@ require_relative 'rna_transcription'
 
 class ComplementTest < Minitest::Test
   def test_rna_complement_of_cytosine_is_guanine
-    # skip
     assert_equal 'G', Complement.of_dna('C')
   end
 
   def test_rna_complement_of_guanine_is_cytosine
-    skip
     assert_equal 'C', Complement.of_dna('G')
   end
 
   def test_rna_complement_of_thymine_is_adenine
-    skip
     assert_equal 'A', Complement.of_dna('T')
   end
 
   def test_rna_complement_of_adenine_is_uracil
-    skip
     assert_equal 'U', Complement.of_dna('A')
   end
 
