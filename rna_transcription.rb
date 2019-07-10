bbfe03c7abafda4dd97489e8365dddd3a1d35b65
Juliette Audema <juliette.audema@gmail.com>
2019-07-10 20:27:01 +0200

    Test 5

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,11 +1,19 @@
 class Complement
-  def self.of_dna(nucleotide)
-    case nucleotide
+  def self.of_dna(nucleotide_chain)
+    array_of_nucleotides = []
+    nucleotide_chain.each_char do |c|
+      array_of_nucleotides.push(rna_translation(c))
+    end
+    array_of_nucleotides.join
+  end
+
+  def self.rna_translation(char)
+    case char
     when 'G' then 'C'
     when 'C' then 'G'
     when 'T' then 'A'
     when 'A' then 'U'
     end
   end
-  
+ 
 end
