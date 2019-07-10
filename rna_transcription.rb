b552198207484f4dfef300bd3174555e559fc139
Fabienne <fabienne@fidzup.com>
2019-07-10 21:30:28 +0200

    simple quote

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,11 +1,11 @@
 class Complement
   def self.of_dna(nucleotide_chain) 
-    return "" if nucleotide_chain =~ /[^AGTC]/
+    return '' if nucleotide_chain =~ /[^AGTC]/
     nucleotide_chain.tr('GCTA', 'CGAU')
   end
 
   # Other way to do it : 
-  
+
   # def self.of_dna(nucleotide_chain) 
   #   dna_array = nucleotide_chain.chars
   #   rna_array = nucl_array.each_with_object([]) { |char,arr| arr.push rna_translation(char) }
