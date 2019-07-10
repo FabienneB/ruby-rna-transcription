acb018bbd134c382e781cf8a91089333945c53cb
Fabienne <fabienne@fidzup.com>
2019-07-10 21:29:48 +0200

    add new way to resolve the kata

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,18 +1,23 @@
 class Complement
   def self.of_dna(nucleotide_chain) 
     return "" if nucleotide_chain =~ /[^AGTC]/
-
     nucleotide_chain.tr('GCTA', 'CGAU')
-    # nucleotide_chain.chars.each_with_object([]) {|char,arr| arr.push rna_translation(char)}.join
   end
 
-  def self.rna_translation(char)
-    case char
-    when 'G' then 'C'
-    when 'C' then 'G'
-    when 'T' then 'A'
-    when 'A' then 'U'
-    end
-  end
- 
+  # Other way to do it : 
+  
+  # def self.of_dna(nucleotide_chain) 
+  #   dna_array = nucleotide_chain.chars
+  #   rna_array = nucl_array.each_with_object([]) { |char,arr| arr.push rna_translation(char) }
+  #   rna_array.join
+  # end
+
+  # def self.rna_translation(char)
+  #   case char
+  #   when 'G' then 'C'
+  #   when 'C' then 'G'
+  #   when 'T' then 'A'
+  #   when 'A' then 'U'
+  #   end
+  # end
 end 
